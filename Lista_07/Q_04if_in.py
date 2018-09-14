46cdf51be517a4c83bc2cc21f389cd1f0583493b
camumbembe <rafaellamgsouza@gmail.com>
2018-09-13 21:55:57 -0300

    exercicio 4 com in dentro do if

---
+++ Lista_07/Q_04if_in.py
@@ -0,0 +1,14 @@
+
+
+palavra = input('Informe uma palavra e vamos contar suas vogais e consoantes: ')
+
+todasvogais = 'AEIOUaeiouãõÃÕáéíóúÁÉÍÓÚ'
+vogais = consoantes = 0
+
+for letra in palavra:
+	if letra in todasvogais:
+		vogais = vogais + 1
+	else:
+		consoantes = consoantes + 1
+
+print('Vogais = {0} e Consoantes = {1}'.format(vogais,consoantes))
