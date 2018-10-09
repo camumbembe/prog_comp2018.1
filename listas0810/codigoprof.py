0182efb0224b83ab5e47bedfdad0c0da3b650759
camumbembe <rafaellamgsouza@gmail.com>
2018-10-08 21:26:21 -0300

    Exercicio de lista com nomes, notas e media usando append e format, codigo do professor com alteracoes propostas em sala pra uma nova impressao

---
+++ listas0810/codigoprof.py
@@ -0,0 +1,56 @@
+# Importando a biblioteca para execução de comandos SHELL
+import os
+
+# Lipando a Tela
+os.system('clear')
+
+# Declarando as variáveis
+vetNomes = []
+vetNotas = []
+qtAlunos = 5
+somaNotas = 0
+
+for contador in range(0, qtAlunos):
+   # Lendo o nome do aluno
+   nome = input('Informe o nome do aluno: ')
+   # Inserindo o nome do aluno na lista de nomes
+   vetNomes.append(nome)
+
+   # Lendo a nota do aluno
+   nota = float(input('Informe a nota do aluno {0}: '.format(vetNomes[contador])))
+   vetNotas.append(nota)
+
+   print('')
+
+   somaNotas = somaNotas + vetNotas[contador]
+
+
+# Lipando a Tela
+os.system("clear")
+
+# Exibindo a média da turma
+mediaTurma = somaNotas/qtAlunos
+print('=====================================================================')
+print('A média da turma foi {0:.1f}'.format(mediaTurma))
+print('=====================================================================')
+
+# Exibindo a lista de Alunos com média acima da média da turma 
+print('Nome do Aluno                                           Nota do Aluno')
+for contador in range(0, qtAlunos):
+	if (vetNotas[contador] >= mediaTurma):
+		print('{0:30} {1:.1f} Ok!'.format(vetNomes[contador].upper(), vetNotas[contador]))
+		print('---------------------------------------------------------------------')
+	else:
+		print('{0:30} {1:.1f} Erro!'.format(vetNomes[contador].upper(), vetNotas[contador]))
+		print('---------------------------------------------------------------------')
+print('=====================================================================')
+
+
+
+
+
+
+
+
+
+
