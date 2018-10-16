b2470210ac6bfcd19f680836914e5083d455319b
camumbembe <rafaellamgsouza@gmail.com>
2018-10-15 21:11:42 -0300

    lista primos feita

--- Lista_08/primosnalista.py
+++ Lista_08/primosnalista.py
@@ -1,16 +1,18 @@
 
 
 lista_primos = [2]
-contador = 0
-posicao = 2
+condicao = 0
 
 
-for element in range(3, 101,1):
-    if element % lista_primos[posicao] == 0:
-        contador == 0
-    else:
-        contador +=1
-        lista_primos.append(element)
+for element in range(3, 101,2):
+	for numero in lista_primos:
+		if element % numero != 0:
+			condicao = True
+		else:
+			condicao = False
+			break
+	if condicao == True:
+		lista_primos.append(element)
 
    
 print(lista_primos)
