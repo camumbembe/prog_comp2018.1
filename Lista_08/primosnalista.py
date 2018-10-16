f4bb0142670608856c7effe07e0cf92ba2fbf528
Rafaella Souza <39624306+camumbembe@users.noreply.github.com>
2018-10-16 08:23:30 -0300

    renomeando arquivos aula while

--- Lista_08/primosnalista.py
+++ Lista_08/primosnalista.py
@@ -2,15 +2,13 @@
 
 lista_primos = [2]
 contador = 0
-posicao = 2
+posicao = 0
 
 
 for element in range(3, 101,1):
-    if element % lista_primos[posicao] == 0:
-        contador == 0
-    else:
-        contador +=1
+    if element % 2 != 0 and element % 3 !=0:
         lista_primos.append(element)
 
-   
+
+  
 print(lista_primos)
