dc7f98c47634e370aeb8e247cb2d42cf9f8cae14
camumbembe <rafaellamgsouza@gmail.com>
2018-10-24 20:16:36 -0300

    lista 09 em sala terminar em casa

---
+++ Lista_09/Q02.py
@@ -0,0 +1,19 @@
+
+import random
+
+n = int(input('Informe o valor de n: '))
+
+lista_n = []
+
+quartil1 = []
+quartil2 = []
+quartil3 = []
+quartil4 = []
+
+
+for element in range(0, n):	
+	lista_n.append(random.randint(0,99))
+
+#criar um for para cada quartil e verficar se os elemento estão presentes e contar quantos estão resentes
+
+
