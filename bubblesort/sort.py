1da49634b5e7064fc6a65512b3c3b6ff6cc7b92b
Rafaella Souza <39624306+camumbembe@users.noreply.github.com>
2018-10-29 18:47:47 -0300

    bubble funcionando

--- bubblesort/sort.py
+++ bubblesort/sort.py
@@ -1,28 +1,47 @@
 from os import system
 system('clear')
-
 import random
 
+n = int(input('Informe o valor de n: '))
 lista = []
-for element in range(10):
-    element = random.randint(1,50)
-    lista.append(element)
+
+for element in range(0, n):	
+    lista.append(random.randint(1,50))
+
 
 print(lista)
 
 
-n = 0
+a = 1
+contador = 0
+condicao = True
 
+while condicao == True:
+    condicao = False
+    a = 1
+    for position in range(n-1):
+        if lista[position] > lista[a]:
+            lista[position], lista[a] = lista[a], lista[position]  
+            print(lista)
+            contador += 1
+            condicao = True
+        a += 1
+
+print(contador)
+print(lista)    
 
 # for element in lista:
-    
-#     if element < lista[n]:
-#         element[n], lista[n+1] = element[n+1], lista[n]    
+#     if element > lista[a]:
+#         lista[a-1], lista[a] = lista[a], lista[a-1]  
 #         print(lista)
-#     n+=1
+#         contador += 1
+#     a += 1
+    
 
-while (n < len(lista)):
-    if element < lista[n]:
-        element[n], lista[n+1] = element[n+1], lista[n]    
-        print(lista)
-    n+=1
+
+
+# while (a < len(lista)):
+#     if element < lista[a]:
+#         element[a], lista[a+1] = element[a+1], lista[a]    
+#         print(lista)
+#     a +=1 
