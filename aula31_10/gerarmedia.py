078c1dc9e2831ff7775f96ce2a86d18b26366b0c
camumbembe <rafaellamgsouza@gmail.com>
2018-10-31 20:38:16 -0300

    aula sobre manupulação de arquivos

---
+++ aula31_10/gerarmedia.py
@@ -0,0 +1,16 @@
+
+import statistics 
+
+arquivo = open('notas_alunos.txt', 'r')
+lista_notas - []
+conteudo  = arquivo.readline()
+lista_notas.append(conteudo[:-1])
+
+
+
+
+arquivo.close()
+
+print(lista_notas)
+
+	
