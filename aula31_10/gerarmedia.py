f63e98df2cf86207dc000d5157de6262be6ca257
camumbembe <rafaellamgsouza@gmail.com>
2018-11-01 21:56:20 -0300

    continuacao manipulacao de arquivos, terminar o codigo

--- aula31_10/gerarmedia.py
+++ aula31_10/gerarmedia.py
@@ -2,15 +2,29 @@
 import statistics 
 
 arquivo = open('notas_alunos.txt', 'r')
-lista_notas - []
+
+lista_notas = []
+
 conteudo  = arquivo.readline()
-lista_notas.append(conteudo[:-1])
+linha = conteudo[:-1].split(';')
+lista_notas.append(linha)
 
-
+while conteudo:
+	conteudo = arquivo.readline()
+	linha = conteudo[:-1].split(';')
+	lista_notas.append(linha)
 
 
 arquivo.close()
 
-print(lista_notas)
+lista_notas.pop()
+
+for auxiliar in range(0, len(lista_notas)):
+matricula = 
+nota1 = float(
+nota2 = 
+nota3=
+nota4 =
+media = (nota1+nota2+nota3 +nota4)/4
 
 	
