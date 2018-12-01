0c68ee53ba9500d41f7492175a3508abfe97296f
camumbembe <rafaellamgsouza@gmail.com>
2018-12-01 08:23:30 -0300

    arquivo editado

--- scope/nunosso.py
+++ scope/nunosso.py
@@ -1,16 +1,54 @@
+import os
+os.system('clear')
+
 analisar = open('nubank.ofx', 'r')
 
 linhas = open('linhas.txt', 'w')
+parcelado = open('parcelado.txt', 'w')
+
+total = 0
 
 for linha in analisar:
     if '<TRNAMT>' in linha:
-        valor = (linha[8:])
-    if '<MEMO>' in linha:
-        resultado = linha[6:] + ':' + valor 
-        linhas.write(resultado) 
-            
-linhas.close()
+        valor = (linha[8:-1]) #remover o negativo (como fazer com os créditos?)
+        valor = float(valor) * (-1)
 
+    if '<MEMO>' in linha and '/' not in linha and 'Pagamento recebido' not in linha:
+        resultado = linha[6:-1] + ' : ' + str(valor) + '\n'
+        total = total + float(valor)
+        linhas.write(resultado)
+ 
+
+    elif '<MEMO>' in linha and '/' in linha:
+        barra = linha.find('/')
+               
+        if linha[barra-2] == '':
+            pqt = linha[barra -1]
+
+        else:
+            pqt = linha[barra-2:barra]
+
+        if linha[barra+2] == '':
+            qpf = linha[barra +1]
+
+        else:
+            qpf = linha[barra+1:barra+3]
+        
+        r_parcelas = int(qpf) - int(pqt)
+        t_parcelas = r_parcelas * valor
+     
+        parcelas = linha[6:-1] + ' : ' + str(valor) + ' ' +  str(t_parcelas) + '\n'
+        total = total + float(valor)
+        parcelado.write(parcelas) 
+
+    
+
+
+
+
+parcelado.close()     
+    
+linhas.close()
 
 analisar.close()
 
--- scope/parcelado.py
+++ scope/parcelado.py
@@ -1,13 +1,17 @@
+import os
+os.system('clear')
+
 analisar = open('nubank.ofx', 'r')
 
 parcelado = open('parcelado.txt', 'w')
 
 for linha in analisar:
     if '<TRNAMT>' in linha:
-        valor = (linha[8:])
-    if ('<MEMO>' and '/') in linha:
-        parcelado = linha[6:] + ':' + valor
-        linhas.write(parcelado) 
+        valor = (linha[8:-1])
+
+    if '<MEMO>' in linha and '/' in linha:
+        parcelas = linha[6:-1] + ':' + valor + '\n'
+        parcelado.write(parcelas) 
 
 parcelado.close()
 
